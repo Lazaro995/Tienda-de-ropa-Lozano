1ecec08412986a30402b24d146d51583aab2d31b
Martin Lozano <martin.lozano995@gmail.com>
2022-08-20 23:51:35 -0300

    Agregando Counter.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import NavBar from './Components/NavBar/NavBar';
+import Counter from './Components/Counter/Counter';
 function App() {
   return (
  <div className='App'>
   <NavBar/>
   <ItemListContainer greeting= "Hola, estoy pasando datos desde mi app.js"/>
+  <Counter/>
      </div>
   );
 }
