ff3f59ec2d4197f86038b74afc1b4d5f2499b7c2
Martin Lozano <martin.lozano995@gmail.com>
2022-08-21 15:09:19 -0300

    Quitando errores y agregando la resolución

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import NavBar from './Components/NavBar/NavBar';
-import Counter from './Components/Counter/Counter';
+import { Contador } from './Components/Contador/Contador';
 function App() {
+  const onAdd= () =>{
+    console.log ("Soy onAdd")
+  }
   return (
  <div className='App'>
   <NavBar/>
   <ItemListContainer greeting= "Hola, estoy pasando datos desde mi app.js"/>
-  <Counter/>
+  <Contador stock={5} initial={1} onAdd={0}/>
      </div>
   );
 }
---
+++ src/Components/Contador/Contador.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export const Contador = ({ stock, initial = 1, onAdd }) => {
+    const [contador, setContador] = useState(initial)
+
+    const aumentarContador = () => {
+        if (contador < stock) setContador(contador + 1)
+    }
+    const disminuirContador = () => {
+        if (contador > initial) setContador(contador - 1)
+    }
+
+
+return (
+    <>
+        <button onClick={disminuirContador}>-</button>
+        <span>{contador}</span>
+        <button onClick={aumentarContador}>+</button>
+    </>
+);
+}
