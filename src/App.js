1b25760397991efc90e4851b585a3f4495fe5a79
Martin Lozano <martin.lozano995@gmail.com>
2022-09-01 21:31:37 -0300

    Crasheando la pagina

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import './App.css';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import NavBar from './Components/NavBar/NavBar';
 import { Contador } from './Components/Contador/Contador';
+import {ItemDetailContainer} from './Components/ItemDetailContainer/ItemDetailContainer';
+
 function App() {
-  const onAdd = (contador)=>{
-    console.log ("Soy onAdd y el valor del contador es:", contador)
+  const onAdd = (contador) => {
+    console.log("Soy onAdd y el valor del contador es:", contador)
   }
   return (
- <div className='App'>
-  <NavBar/>
-  <ItemListContainer greeting= "Hola, estoy pasando datos desde mi app.js"/>
-  <Contador stock={5} initial={0} onAdd={onAdd}/>
-     </div>
+    <div className='App'>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer greeting="Hola, estoy pasando datos desde mi app.js" />} />
+          <Route path='/item/:id' element={<ItemDetailContainer/>} />
+
+          
+        </Routes>
+      </BrowserRouter>
+      <Contador stock={5} initial={0} onAdd={onAdd} />
+    </div >
   );
 }
 
